f5758b04c93ad11a1f0d4c2c9b4f01d6fb790027
Ahmad Zaki <166486850+ahmadzakiyox@users.noreply.github.com>
2025-08-29 14:29:00 +0700

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,17 +1,6 @@
-// models/Admin.js
 const mongoose = require('mongoose');
-
 const adminSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    username: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true }
 });
-
 module.exports = mongoose.model('Admin', adminSchema);
